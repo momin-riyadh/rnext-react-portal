f193cee8088964a232fe634b295ccfafde27d279
Momin Riyadh <mominriyadh@gmail.com>
2024-03-05 12:22:31 +0600

    fix and deploy portal modal

--- src/Portal.jsx
+++ src/Portal.jsx
@@ -1,9 +1,18 @@
 import {createPortal} from "react-dom";
+import {useEffect} from "react";
 
 function Portal({children}) {
     const mountElement = document.getElementById('portal-root');
+    const elementDiv = document.createElement('div');
+    useEffect(() => {
+        mountElement.appendChild(elementDiv);
+
+        return () => mountElement.removeChild(elementDiv);
+
+    }, [elementDiv, mountElement]);
+
     return (
-        createPortal(children, mountElement)
+        createPortal(children, elementDiv)
     );
 }
 
