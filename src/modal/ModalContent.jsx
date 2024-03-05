e166c120e9753b4730ed7e95892fb5491f1e5891
Momin Riyadh <mominriyadh@gmail.com>
2024-03-05 12:22:48 +0600

    fix and deploy portal modal

--- src/modal/ModalContent.jsx
+++ src/modal/ModalContent.jsx
@@ -1,8 +1,7 @@
-
-
 function ModalContent({onClose}) {
     return (
-        <div className="flex justify-evenly items-center border rounded-lg absolute top-20 left-12 bottom-20 z-10 bg-black p-2">
+        <div
+            className="flex justify-evenly items-center border rounded-lg absolute top-20 left-12 bottom-20 z-10 bg-black p-2">
             <p className="mx-2 text-white">Hey I am Modal</p>
             <button className="bg-white text-black p-1 border rounded-md" onClick={onClose}>Close</button>
         </div>
